0b37b2046e13ea68a061ef68b77e7b1fea27eaad
Jacob Rodgers <yudjinncoding@gmail.com>
2020-07-22 15:31:45 -0600

    Added models, fixed admin profiles, updated README, moved Secret Key to be untracked

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-# onefit
-Fitness Tracker/Logger/Planner
+# onefit : Fitness Tracker/Logger/Planner
+django backend with (planned) react frontend
+
+# to run:
+create file `secret_settings.py` next to settings.py
+File should inclue:
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = 'secretHere'
+
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = True
+
+Obtain a secret key from [Djecrety](https://djecrety.ir/) and replace `secretHere`
 
 default admin account: admin
-password:
+
+password:onefit
--- onefit/users/models.py
+++ onefit/users/models.py
@@ -5,15 +5,39 @@ from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    UNITS_OPT = (
+        ('LBS','Pounds'),
+        ('KG','Kilograms'),
+    )
+    units = models.CharField(max_length=3, choices=UNITS_OPT)
 
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
         #Image resizing
         img = Image.open(self.image.path)
         if img.height > 300 or img.width > 300:
             output_size=(300,300)
             img.thumbnail(output_size)
             img.save(self.image.path)
+
+
+class Workout(models.Model):
+    date = models.DateTimeField()
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    notes = models.TextField()
+
+class Exercises(models.Model):
+    name = models.CharField(max_length=50)
+
+class Exercise(models.Model):
+    workout_id = models.ForeignKey(Workout, on_delete=models.CASCADE)
+    order = models.IntegerField(unique=True)
+    exercise_id = models.ForeignKey(Exercises, on_delete=models.CASCADE)
+
+class Set(models.Model):
+    order = models.IntegerField(unique=True)
+    weight = models.FloatField()
+    reps = models.IntegerField()
